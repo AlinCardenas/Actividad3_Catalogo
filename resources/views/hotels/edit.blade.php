e4caea5eb518156e06f96c9e69676442dc74aa99
Alin Cardenas <al222110745@gmail.com>
2023-05-13 21:15:38 -0600

    Posterior

--- resources/views/hotels/edit.blade.php
+++ resources/views/hotels/edit.blade.php
@@ -26,7 +26,7 @@
     
         <div class="mb-4">
             <label for="description" class="block mb-2 font-bold">Descripcion</label>
-            <textarea name="description" class="w-full px-3 py-2 border rounded-md" >{{$hotel->description}}</textarea>
+            <textarea name="description" class="w-full px-3 py-2 border rounded-md" >value="{{$hotel->description}}"</textarea>
         </div>
 
             <div class="grid grid-cols-4 gap-3">
