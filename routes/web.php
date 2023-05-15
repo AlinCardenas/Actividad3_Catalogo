37c55c623223646b7b580af75cec4a98a967bb6d
RafaEscobar <Rafael.EG.code@gmail.com>
2023-05-14 19:55:23 -0600

    Avance y siguiente para show concluido

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DestinationsController;
 use App\Http\Controllers\HotelsController;
 use App\Http\Controllers\PlaneController;
 use App\Http\Controllers\SendEmailController;
+use App\Models\Destination;
 use App\Models\Hotel;
 use Illuminate\Support\Facades\Route;
 
@@ -40,6 +41,8 @@ Route::post('enviar', [SendEmailController::class, 'envio'])->name('enviar');
 
 // Ruta de destinos
 Route::resource('destinations', DestinationsController::class);
+Route::get('destination/show-skip/{id}', [DestinationsController::class, 'skip'])->name('destinations.skip');
+Route::get('destination/show-back/{id}', [DestinationsController::class, 'back'])->name('destinations.back');
 
 //Ruta para crud de hoteles
 Route::resource('hotels', HotelsController::class);
