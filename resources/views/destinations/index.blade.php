725d9e6ecae26652da153f7a6be25865ca1e1f35
RafaEscobar <Rafael.EG.code@gmail.com>
2023-05-17 17:49:03 -0600

    Listado de destinos

--- resources/views/destinations/index.blade.php
+++ resources/views/destinations/index.blade.php
@@ -1,47 +1,39 @@
-<x-layout>
-    <x-table title="Registros de destinos">
-        <x-slot name="btn">
-            <x-link-id href="{{route('destinations.create')}}" mensaje="Crear destino"
-                class="bg-slate-950 rounded-sm w-[165px] text-center" />
-        </x-slot>
 
-        <table class="border-collapse border-4 border-cyan-500 w-full mt-5">
-            <thead>
-                <tr>
-                    <x-table-head mensaje="Nombre" />
-                    <x-table-head mensaje="Valoración" />
-                    <x-table-head mensaje="Descripción" />
-                    <x-table-head mensaje="Idiomas" />
-                    <x-table-head mensaje="Mostrar" />
-                    <x-table-head mensaje="Editar" />
-                    <x-table-head mensaje="Eliminar" />
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($registers as $item)
-                <tr>
-                    <x-table-row :item="$item->name" />
-                    <x-table-row :item="$item->ranking" />
-                    <x-table-row :item="Str::limit($item->description, 30)" />
-                    <x-table-row :item="$item->languages" />
-
-                    <x-table-row-link>
-                        <x-link-id href="{{route('destinations.show', $item->id)}}" mensaje="Mostrar" />
-                    </x-table-row-link>
-                    <x-table-row-link>
-                        <x-link-id href="{{route('destinations.edit', $item->id)}}" mensaje="Editar"
-                            class="bg-green-600" />
-                    </x-table-row-link>
-                    <x-table-row-link>
-                        <x-table-row-delete action="{{route('destinations.destroy', $item->id)}}" />
-                    </x-table-row-link>
-                </tr>
-                @endforeach
-
-            </tbody>
-        </table>
-        <span class="m-8">
-            {{ $registers->links() }}
-        </span>
-    </x-table>
-</x-layout>
+<x-guest-layout>
+    <div class="container max-w-[7xl] mx-auto mt-4">
+        <h1 class="text-3xl text-center font-bold ">Lista de destinos</h1>
+        <div class="flex justify-end mt-2">
+            <a href="{{ route('destinations.create') }}" class="font-bold text-purple-500 hover:text-purple-600 px-4"> Agregar destino</a>
+        </div>
+        <div class="grid grid-cols-4 gap-4 mb-6 mx-auto mt-5">
+            @foreach ($registers as $item)
+            <div class="col-span-1">
+                <div class="bg-white rounded-lg shadow-2xl p-[10px] h-[500px]">
+                    @php
+                    // 1.Rempazar nombre imagen
+                    $ruta_logo = str_replace('public/img/', '', $item->images);
+                    @endphp
+                    <img class="mx-auto mt-3" src="{{asset('storage/img/'.$ruta_logo)}}" width="250" height="250">
+                    <div class="p-4">
+                        <a class="text-xl font-bold mb-4 hover:text-purple-500" href="{{route('destinations.show', $item->id)}}">Destino {{$item->name}} </a>
+                        <p class="text-gray-600 mb-2">Puntuación: {{ $item->ranking }}</p>
+                        <p class="text-gray-600 mb-2">Descripción: {{ Str::limit($item->description, 60) }}</p>
+                        <div class="mt-2">
+                            <a href="{{ route('destinations.edit', $item->id) }}" class="font-bold text-blue-600 hover:text-blue-700">Editar</a>
+                        </div>
+                        <form action="{{ route('destinations.destroy', $item->id) }}" method="post" class="inline">
+                            @csrf
+                            @method('delete')
+                            <div class="mt-2">
+                                <button type="submit"
+                                    class="font-bold text-rose-600 hover:text-red-700">Eliminar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        {{$registers->links()}}
+    </div>
+</x-guest-layout>
