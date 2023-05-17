9052af61dc185923cd26851fda86ff1abf8cacbd
RafaEscobar <Rafael.EG.code@gmail.com>
2023-05-17 14:37:36 -0600

    Detalles de etiquetas

--- resources/views/hotels/edit.blade.php
+++ resources/views/hotels/edit.blade.php
@@ -1,5 +1,6 @@
 <x-guest-layout>
     <div class="container max-w-6xl mx-auto mt-4">
+        <h1 class="text-3xl text-center font-bold  text-gray-700">Actualizar registro</h1>
         <form action="{{route('hotels.update', $hotel)}}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('put')
