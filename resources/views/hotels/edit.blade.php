2ce15cb7c9393e89a7f2582b6440c8821db7b99c
RafaEscobar <Rafael.EG.code@gmail.com>
2023-05-17 14:32:38 -0600

    Indentación

--- resources/views/hotels/edit.blade.php
+++ resources/views/hotels/edit.blade.php
@@ -1,13 +1,9 @@
 <x-guest-layout>
     <div class="container max-w-6xl mx-auto mt-4">
-
         <form action="{{route('hotels.update', $hotel)}}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('put')
-            @include('hotels.form')
-            
+            @include('hotels.form')            
         </form>
-
     </div>
-
 </x-guest-layout>
--- resources/views/hotels/showAll.blade.php
+++ resources/views/hotels/showAll.blade.php
@@ -1,17 +1,13 @@
 <x-guest-layout>
-    
     <div class="container max-w-[7xl] mx-auto mt-4">
         <h1 class="text-3xl text-center font-bold ">Lista de hoteles</h1>
-
         <div class="grid grid-cols-4 gap-4 mb-6 mx-auto mt-5">
             @foreach ($hotels as $hotel)
-                <x-card :hotel="$hotel">
-                    {{-- Cosas --}}
-                </x-card>
+            <x-card :hotel="$hotel">
+                {{-- Cosas --}}
+            </x-card>
             @endforeach
-        </div> 
-        
+        </div>
         {{$hotels->links()}}
-
     </div>
 </x-guest-layout>
