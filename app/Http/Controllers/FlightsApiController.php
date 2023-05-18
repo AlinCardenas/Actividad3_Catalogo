d00194de76949fb367cbd8bd5e2c67aab17978ad
Alin Cardenas <al222110745@gmail.com>
2023-05-18 13:50:25 -0600

    API vuelos funcionando

---
+++ app/Http/Controllers/FlightsApiController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Flight;
+use Illuminate\Http\Request;
+
+class FlightsApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $flights = Flight::paginate(20);
+        return response()->json($flights);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $registro = new Flight($request->input());
+        $registro->save();
+        return response()->json([
+            'status'=>true,
+            'message'=>'Registro guardado',
+            'data'=>$registro,
+        ]);
+        /* dump($registro); */
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $resp =  Flight::find($id);
+        return response()->json([
+            'status'=>true,
+            'data'=>$resp,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $up = Flight::find($id);
+        $up->update($request->input());
+        return response()->json([
+            'status'=>true,
+            'message'=>'Registro actualizado',
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $del = Flight::find($id);
+        $del->delete();
+        return response()->json([
+            'status'=>true,
+            'message' =>'Registro eliminado',
+        ]);
+    }
+}
--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -9,6 +9,17 @@ class Flight extends Model
 {
     use HasFactory;
 
+    protected $fillable=[
+        'leave_date',
+        'arrive_date',
+        'count_clients',
+        'duration',
+        'price',
+        'type_lunggage',
+        'plane_id',
+        'airline_destination_id',
+    ];
+
     public function planes(){
         return $this->hasOne('App\Models\Plane', 'id', 'plane_id');
     }
