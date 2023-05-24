14620060db4b544d1cbf8956ed88653eaf093b92
RafaEscobar <Rafael.EG.code@gmail.com>
2023-05-24 12:16:22 -0600

    Modificacion de apis #2

--- app/Http/Controllers/DestinationApiController.php
+++ app/Http/Controllers/DestinationApiController.php
@@ -12,7 +12,7 @@ class DestinationApiController extends Controller
      */
     public function index()
     {
-        $destination = Destination::all()->with('addresses')->get();
+        $destination = Destination::select('*')->with('addresses')->get();
         return response()->json($destination);
     }
 
